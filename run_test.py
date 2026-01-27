cf868bd60f308a82e75e3eabfeb14320efbe5dc9
Jeff Penberton-Mola <jpenberton7@gmail.com>
2026-01-28 00:15:27 +0100

    refactor: remove logs and test scripts from tracking

--- run_test.py
+++
@@ -1,7 +0,0 @@
-from core.logger import get_logger
-
-# Initialize the logger
-log = get_logger("System-Check")
-
-log.info("The Growth-Automation-Stack is alive!")
-log.warning("This is a test of the emergency alert system.")
